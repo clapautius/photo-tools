cf6e929d7aea0c4c369deb2d413e5cec2762844e
Tudor M. Pristavu <clapautius@gmail.com>
2011-02-03 20:01:47 +0000

    Added first files.

---
+++ photoArchiveQt/photoArchiveQt.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+#include <QApplication>
+#include <QMessageBox>
+#include <QGroupBox>
+#include <QSettings>
+#include <QDir>
+
+#include "photoArchiveQt.h"
+
+using std::vector;
+using std::map;
+using std::endl;
+
---
+++ photoArchiveQt/photoArchiveQt.h
@@ -0,0 +1,37 @@
+#ifndef PHOTO_ARCHIVE_QT_H
+#define PHOTO_ARCHIVE_QT_H
+
+#include <map>
+#include <vector>
+
+#include <QDialog>
+#include <QBoxLayout>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QTextEdit>
+#include <QFileInfo>
+#include <QCloseEvent>
+#include <QLabel>
+
+#define PHOTO_ARCHIVE_QT_VER "0"
+
+class PhotoArchiveWnd : public QDialog
+{
+  Q_OBJECT
+
+public:
+
+protected:
+
+  
+protected slots:
+
+
+private:
+
+};
+
+
+#endif
