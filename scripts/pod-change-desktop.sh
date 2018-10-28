e8a2f0e74ee793ac0e6e4df8cf97032b5ee4d4cf
Tudor M. Pristavu <clapautius@users.noreply.github.com>
2018-10-28 22:18:47 +0200

    scripts: fix typo in path.

--- scripts/pod-change-desktop.sh
+++ scripts/pod-change-desktop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# ver: 2018-10-28-0
+# ver: 2018-10-28-1
 
 # dimensions taken from plasma-desktop-appletsrc:
 # [Containments][1][Applets][18]
 # geometry=0,35,969,832
 
-echo "Changing desktop"; displayToUse=$(w -s | egrep -m 1 -o "^$USER +([^ ])* +:[0-9]" | egrep -o ":[0-9]$"); [ -n "$displayToUse" ] && "$HOME/bin/photo-collection-pod.pl" && DISPLAY="$displayToUse" ~/bin/CutyCapt --min-width=969 --out="$HOME/var/run/pod/output/output.png" --url="file:///home/me/var/run/pod/photo-pod.html" 1>>/dev/null 2>>/dev/null && echo "New desktop - OK" || echo "CutyCapt error"
+echo "Changing desktop"; displayToUse=$(w -s | egrep -m 1 -o "^$USER +([^ ])* +:[0-9]" | egrep -o ":[0-9]$"); [ -n "$displayToUse" ] && "$HOME/bin/photo-collection-pod.pl" && DISPLAY="$displayToUse" ~/bin/CutyCapt --min-width=969 --out="$HOME/var/run/pod/output/output.png" --url="file:///$HOME/var/run/pod/photo-pod.html" 1>>/dev/null 2>>/dev/null && echo "New desktop - OK" || echo "CutyCapt error"
